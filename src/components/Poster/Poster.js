ec1716e8aabaf6bbe02f4baa2bebe9d37b65cb2c
Seth Burns <seth@sethburns.com>
2023-07-12 15:21:15 -0700

    Finish initial tests.

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -4,7 +4,7 @@ import './Poster.css';
 
 export default function Poster({ title, poster_path, id, findMovie}){
   return (
-    <div>
+    <div className='movie-card'>
       <img className='poster' alt={title + " movie poster"} onClick={() => findMovie(id)} src={poster_path}/>
       <h3>{title}</h3>
     </div>
