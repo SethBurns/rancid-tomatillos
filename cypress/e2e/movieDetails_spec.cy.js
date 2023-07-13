f48e5d602f066e99f9b0a1619535979cf6e2a040
Seth Burns <seth@sethburns.com>
2023-07-13 12:45:10 -0700

    Initial commit

--- cypress/e2e/movieDetails_spec.cy.js
+++ cypress/e2e/movieDetails_spec.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('shows additional details about a movie when the movie is clicked', () => {
+    cy.visit('localhost:3000/1013860')
+    .get(".movie_details_left").contains("h1", "R.I.P.D. 2: Rise of the Damned")
   })
 })
