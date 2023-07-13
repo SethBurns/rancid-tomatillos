e4b83a9a4936b585e21138a6a0a797ace33dcf11
Jamie Caudill <j.caudill7177@gmail.com>
2023-07-13 08:16:30 -0700

    Refactor return statement

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -12,27 +12,29 @@ export default function MovieDetails({ findMovie, selectedMovie, videos }) {
     findMovie(id);
   }, []);
 
-  return (
-    <div className="movie_details" style={{ backgroundImage: `url(${selectedMovie?.backdrop_path})` }}>
-      <div className="btn_div">
-        <Link to="/"><button className="btn_exit">X</button></Link>
+  if (selectedMovie) {
+    return (
+      <div className="movie_details" style={{ backgroundImage: `url(${selectedMovie?.backdrop_path})` }}>
+        <div className="btn_div">
+          <Link to="/"><button className="btn_exit">X</button></Link>
+        </div>
+        <div className="movie_details_left">
+          <h1>{selectedMovie.title}</h1>
+          <img alt="movie poster" src={selectedMovie.poster_path} width="300px" />
+          <p>{selectedMovie.tagline}</p>
+        </div>
+        <div className="movie_details_middle">
+          <p>Overview: {selectedMovie.overview}</p>
+        </div>
+        <div className="movie_details_right">
+          <p>Average Rating: {selectedMovie.average_rating.toFixed(2)}</p>
+          <p>Release Date: {selectedMovie.release_date}</p>
+          <p>Budget: ${selectedMovie.budget.toLocaleString()}</p>
+          <p>Revenue: ${selectedMovie.revenue.toLocaleString()}</p>
+          <p>Profit: ${(selectedMovie.revenue - selectedMovie.budget).toLocaleString()}</p>
+          <VideoLinks videos={videos} />
+        </div>
       </div>
-      <div className="movie_details_left">
-        <h1>{selectedMovie?.title}</h1>
-        <img alt="movie poster" src={selectedMovie?.poster_path} width="300px" />
-        <p>{selectedMovie?.tagline}</p>
-      </div>
-      <div className="movie_details_middle">
-        <p>Overview: {selectedMovie?.overview}</p>
-      </div>
-      <div className="movie_details_right">
-        <p>Average Rating: {selectedMovie?.average_rating?.toFixed(2)}</p>
-        <p>Release Date: {selectedMovie?.release_date}</p>
-        <p>Budget: ${selectedMovie?.budget?.toLocaleString()}</p>
-        <p>Revenue: ${selectedMovie?.revenue?.toLocaleString()}</p>
-        <p>Profit: ${(selectedMovie?.revenue - selectedMovie?.budget)?.toLocaleString()}</p>
-        <VideoLinks videos={videos} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
